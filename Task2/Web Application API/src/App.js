676fa1807fcc710a40b8b95e54e1e0912355dbf4
FreakWolf <rohitskt115@gmail.com>
2022-08-26 12:54:00 +0530

    second commit

---
+++ Task2/Web Application API/src/App.js
@@ -0,0 +1,94 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+
+  let [state, setState] = useState(true)
+  let [loading, setLoading] = useState(false)
+
+  var pg = 1;
+  const getUsers = function (page) {
+    var file = `https://reqres.in/api/users?page=1`
+    return (
+      fetch(file)
+        .then(x => x.text())
+        .then(
+          y => {
+            var str = JSON.parse(y)
+            var total = str.total_pages
+            var html = function () {
+              for (const i of str.data) {
+                document.getElementById('row').innerHTML += `
+                  <div class="card">
+                      <img src="${i.avatar}" alt="${i.avatar}"></img>
+                      <div class="desc">
+                        <p><strong>Id: </strong>${i.id}</p>
+                        <p><strong>Email: </strong>${i.email}</p>
+                        <p><strong>First Name: </strong>${UpdateString(i.first_name)}</p>
+                        <p><strong>Last Name: </strong>${UpdateString(i.last_name)}</p>
+                      </div>
+                  </div>
+              `
+              }
+            }
+            html()
+            
+            if (page == total)
+              document.getElementById('next-users').style.display = 'none'
+            else
+              document.getElementById('next-users').style.display = 'inline-block'
+          },
+        )
+    )
+
+    function UpdateString(str) {
+      return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase()
+    }
+  }
+
+  function display() {
+    setLoading(true)
+    setState(state = !state)
+    setTimeout(() => {
+      getUsers(pg)
+      setLoading(false)
+    }, 6000)
+  }
+
+  function nextUsers() {
+    pg += 1
+    setLoading(true)
+    setTimeout(() => {
+      getUsers(pg)
+      setLoading(false)
+    }, 6000)
+  }
+
+  return (
+    <div className="App">
+      <div className="container">
+        <header>
+          <nav id="sticky">Web Application API Call</nav>
+          <div className='buttons'>
+            <button type="button" className="btn" id="get-users" onClick={display}><i class="fa-solid fa-users"></i>{state ? 'Get Users' : 'Hide Users'}</button>
+          </div>
+        </header>
+        <section>
+          {
+            !state ?
+              loading ? <div className='loader'>
+                <div className='loader-glass'></div>
+                <span>Processing...</span>
+              </div> : <div id="users" >
+                <p id="count"></p>
+                <div id="row"></div>
+              </div> : ""
+          }
+        </section>
+      </div>
+    </div>
+  );
+}
+
+export default App;
